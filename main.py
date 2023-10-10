d52633347612938e7cacaf594a9274d08a4cdb44
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-10 22:11:24 +0530

    Add File selection button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #initial imports
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 #constants
 FONT_NAME = "Helvetica"
@@ -15,14 +16,30 @@ root.title('IMAGE WATERMARKING SOFTWARE')
 root.config(padx=25, pady=25, bg=BACKGROUND_COLOR)
 root.geometry("850x700")
 
+#image file path
+img_file = ""
+
+#writing some functions
+def select_file():
+    global img_file
+    img_file = askopenfilename()
+    print(f"✓ User has selected the file: {img_file}")
+    print("...")
 
 
 # Create a Frame with a border to hold the Label
-label_frame = Frame(root, relief="solid", borderwidth=2)
-label_frame.pack(padx=10, pady=10)  # Add padding to control the border width
+# label_frame = Frame(root, relief="solid", borderwidth=2)
+# label_frame.pack(padx=10, pady=10)  # Add padding to control the border width
 #title
-title_label = Label(label_frame, text = "IMAGE WATERMARKER", font=(FONT_NAME, 48, "bold"), fg=TITLE_COLOUR, bg=BACKGROUND_COLOR)
+title_label = Label( text = "IMAGE WATERMARKER", font=(FONT_NAME, 48, "bold"),
+                     fg=TITLE_COLOUR, bg=BACKGROUND_COLOR)
 title_label.grid(column=0, row=0 , rowspan=1, columnspan=3)
+
+#creating button to select the image
+select_img_button = Button(root, text="upload image", font=20, width=15, command=select_file)
+select_img_button.grid(column=0, row=1,columnspan=2,padx=25, pady=25)
+
+
 
 
 
